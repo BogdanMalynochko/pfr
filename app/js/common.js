c6a6c3bd37698471938e55ef8a650f49a98e84c5
BogdanMalynochko <bogdanmalynochko@gmail.com>
2021-08-22 14:18:39 +0300

    my second commit

---
+++ app/js/common.js
@@ -0,0 +1,20 @@
+$('.slider-wrapper').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+  $(document).ready(function() {
+    $('.popup-video').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+  
+      fixedContentPos: false
+    });
+  });
